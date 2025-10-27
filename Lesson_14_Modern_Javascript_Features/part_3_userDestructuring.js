a9d9ccdb76505019949d770a4961c65a6fa95267
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:05:38 -0400

    Add Lesson_14_Modern_Javascript_Features/part_3_userDestructuring.js

---
+++ Lesson_14_Modern_Javascript_Features/part_3_userDestructuring.js
@@ -0,0 +1,11 @@
+const user = {
+    name: "Bob",
+    age: 30,
+    location: "New York"
+};
+
+// Destructuring the object
+const { name, age } = user;
+
+console.log(name); // Output: Bob
+console.log(age);  // Output: 30
