e57e1a192795077af16b4fe91fa4a86ca04b1224
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:08:35 -0400

    Add Lesson_30_Modern_Javascript_For_Reactjs/part_5_fetchDataExample.js

---
+++ Lesson_30_Modern_Javascript_For_Reactjs/part_5_fetchDataExample.js
@@ -0,0 +1,13 @@
+fetch('https://api.example.com/data')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+const fetchData = async () => {
+    try {
+        const response = await fetch('https://api.example.com/data');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
