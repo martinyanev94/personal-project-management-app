5a5da0513818c3fb2c3c6d7c1a53fdc8071aa107
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:06:23 -0400

    Add Lesson_19_Introduction_To_Web_Development_Fundamentals/part_2_GreetingComponent.js

---
+++ Lesson_19_Introduction_To_Web_Development_Fundamentals/part_2_GreetingComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Greeting() {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1 style={{ color: 'blue' }}>Welcome to Web Development!</h1>
+            <p>This is a simple React component.</p>
+        </div>
+    );
+}
+
+export default Greeting;
