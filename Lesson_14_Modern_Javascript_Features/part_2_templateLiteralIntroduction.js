0455e3875f61188245b783a9ec300dd3579860bb
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:05:36 -0400

    Add Lesson_14_Modern_Javascript_Features/part_2_templateLiteralIntroduction.js

---
+++ Lesson_14_Modern_Javascript_Features/part_2_templateLiteralIntroduction.js
@@ -0,0 +1,6 @@
+const name = "Alice";
+const age = 25;
+
+// Using template literals
+const introduction = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(introduction); // Output: Hello, my name is Alice and I am 25 years old.
