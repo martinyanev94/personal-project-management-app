0a856cf683dd5a84377350228eed7ff33bdc0d7c
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:08:45 -0400

    Add Lesson_3_Advanced_Array_Methods_In_Javascript/part_5_array-map-example.js

---
+++ Lesson_3_Advanced_Array_Methods_In_Javascript/part_5_array-map-example.js
@@ -0,0 +1,6 @@
+array.map((currentValue, currentIndex, array) => {
+  // return a new value
+});
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map(num => num * 2);
+console.log(doubled); // Output: [2, 4, 6, 8, 10]
