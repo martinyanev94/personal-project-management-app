f2d256b570710d34afc8690ea913d7c1c571746b
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:09:26 -0400

    Add Lesson_7_Overview_Of_Web_Development_Technologies/part_7_fetchPosts.js

---
+++ Lesson_7_Overview_Of_Web_Development_Technologies/part_7_fetchPosts.js
@@ -0,0 +1,15 @@
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error fetching data:', error));
+async function fetchPosts() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+fetchPosts();
