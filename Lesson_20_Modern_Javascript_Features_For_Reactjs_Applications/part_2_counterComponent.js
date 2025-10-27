5fc11fe16df285cf2f17f49d02e1365b297ca5f9
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:06:45 -0400

    Add Lesson_20_Modern_Javascript_Features_For_Reactjs_Applications/part_2_counterComponent.js

---
+++ Lesson_20_Modern_Javascript_Features_For_Reactjs_Applications/part_2_counterComponent.js
@@ -0,0 +1,20 @@
+const sum = (a, b) => a + b;
+class Counter extends React.Component {
+    constructor() {
+        super();
+        this.state = { count: 0 };
+    }
+
+    increment = () => {
+        this.setState(prevState => ({ count: prevState.count + 1 }));
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.increment}>Increment</button>
+            </div>
+        );
+    }
+}
