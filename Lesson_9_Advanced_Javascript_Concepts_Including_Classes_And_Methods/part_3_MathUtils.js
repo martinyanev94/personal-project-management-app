e59c66a68aade6330c3495fa0e42874f61a79397
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:09:42 -0400

    Add Lesson_9_Advanced_Javascript_Concepts_Including_Classes_And_Methods/part_3_MathUtils.js

---
+++ Lesson_9_Advanced_Javascript_Concepts_Including_Classes_And_Methods/part_3_MathUtils.js
@@ -0,0 +1,12 @@
+class MathUtils {
+    static add(x, y) {
+        return x + y;
+    }
+
+    static subtract(x, y) {
+        return x - y;
+    }
+}
+
+console.log(MathUtils.add(5, 10)); // Output: 15
+console.log(MathUtils.subtract(10, 5)); // Output: 5
