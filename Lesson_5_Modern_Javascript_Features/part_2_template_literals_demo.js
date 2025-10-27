5bb549929a1e7de19f202a501b09ab36abc2f635
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:08:58 -0400

    Add Lesson_5_Modern_Javascript_Features/part_2_template_literals_demo.js

---
+++ Lesson_5_Modern_Javascript_Features/part_2_template_literals_demo.js
@@ -0,0 +1,11 @@
+const name = 'John';
+const age = 30;
+
+// Using template literals
+const greeting = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(greeting);
+const multilineString = `This is a string
+that spans multiple lines
+using template literals.`;
+
+console.log(multilineString);
