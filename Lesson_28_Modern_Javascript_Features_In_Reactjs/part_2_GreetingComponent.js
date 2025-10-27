4ac73e896b48e0ae9233f05b5ca95b4fea5ab5b5
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:08:03 -0400

    Add Lesson_28_Modern_Javascript_Features_In_Reactjs/part_2_GreetingComponent.js

---
+++ Lesson_28_Modern_Javascript_Features_In_Reactjs/part_2_GreetingComponent.js
@@ -0,0 +1,8 @@
+class Greeting extends React.Component {
+    render() {
+        const name = "John";
+        return (
+            <h1>{`Hello, ${name}! Welcome to our site.`}</h1>
+        );
+    }
+}
