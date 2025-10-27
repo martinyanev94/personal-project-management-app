046a03c0fbf509be14932600e8eb55506c65d1e8
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:05:39 -0400

    Add Lesson_14_Modern_Javascript_Features/part_4_combineFruits.js

---
+++ Lesson_14_Modern_Javascript_Features/part_4_combineFruits.js
@@ -0,0 +1,6 @@
+const fruits = ["apple", "banana"];
+const moreFruits = ["orange", "kiwi"];
+
+// Combining arrays
+const allFruits = [...fruits, ...moreFruits];
+console.log(allFruits); // Output: ["apple", "banana", "orange", "kiwi"]
