67bb99ba33104e299310503718fca4c1316f2f9f
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:06:11 -0400

    Add Lesson_17_Introduction_To_Web_Development_Fundamentals/part_5_fetchDataAndLog.js

---
+++ Lesson_17_Introduction_To_Web_Development_Fundamentals/part_5_fetchDataAndLog.js
@@ -0,0 +1,6 @@
+fetch('https://api.example.com/data')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
