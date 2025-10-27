095d7a38bbf3213f978f42c536a42e580b28f4fb
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:08:36 -0400

    Add Lesson_30_Modern_Javascript_For_Reactjs/part_6_mathOperations.js

---
+++ Lesson_30_Modern_Javascript_For_Reactjs/part_6_mathOperations.js
@@ -0,0 +1,6 @@
+export const add = (a, b) => a + b;
+export const subtract = (a, b) => a - b;
+import { add, subtract } from './math';
+
+console.log(add(5, 3)); // Outputs: 8
+console.log(subtract(5, 3)); // Outputs: 2
