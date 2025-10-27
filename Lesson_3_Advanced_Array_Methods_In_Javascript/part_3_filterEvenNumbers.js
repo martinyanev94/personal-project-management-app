18c0b99f9d8b12c78fe322ad69775f5ff8953bfc
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:08:42 -0400

    Add Lesson_3_Advanced_Array_Methods_In_Javascript/part_3_filterEvenNumbers.js

---
+++ Lesson_3_Advanced_Array_Methods_In_Javascript/part_3_filterEvenNumbers.js
@@ -0,0 +1,6 @@
+array.filter((currentValue, currentIndex, array) => {
+  // return true or false
+});
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+console.log(evenNumbers); // Output: [2, 4, 6, 8, 10]
