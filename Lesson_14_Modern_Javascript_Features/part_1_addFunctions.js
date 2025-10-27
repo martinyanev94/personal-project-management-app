1a1d9cb4fdd6756b65f1343eab2b19a5c953b9ba
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-10-26 20:05:35 -0400

    Add Lesson_14_Modern_Javascript_Features/part_1_addFunctions.js

---
+++ Lesson_14_Modern_Javascript_Features/part_1_addFunctions.js
@@ -0,0 +1,10 @@
+// Traditional function
+const add = function(a, b) {
+    return a + b;
+};
+
+// Arrow function
+const addArrow = (a, b) => a + b;
+
+console.log(add(5, 3)); // Output: 8
+console.log(addArrow(5, 3)); // Output: 8
